17d498293543747f4416e12e0be6bf7abd15c4f2
Jeka <Evgeny.Myandin@gmail.com>
2022-05-26 15:38:25 +0300

    Delete gocrossbuild.sh

--- gocrossbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GOOS=darwin GOARCH=amd64 go build -o tcpfw-darwin tcpfw.go
-GOOS=windows GOARCH=amd64 go build -o tcpfw-windows.exe tcpfw.go
-GOOS=freebsd GOARCH=amd64 go build -o tcpfw-freebsd tcpfw.go
-GOOS=linux GOARCH=arm64 go build -o tcpfw-linux-arm64 tcpfw.go
-GOOS=linux GOARCH=amd64 go build -o tcpfw-linux tcpfw.go
